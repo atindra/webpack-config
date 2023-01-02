449c39271bc5c9d6f578f293f585779a77add49a
Atindra Kumar Singh <atindra.singh86@gmail.com>
2023-01-02 18:33:48 -0500

    add react refresh for development

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -9,7 +9,7 @@ const App = () => {
       <section className="hero"></section>
       <main>
         <section>
-          <h1>Oh Hai, React</h1>
+          <h1>Oh Hai</h1>
         </section>
         <img src={sword} alt="sword" width="250" />
         <img src={swordSvg} alt="sword svg" width="250" />
--- src/components/Recipes.jsx
+++ src/components/Recipes.jsx
@@ -1,37 +1,45 @@
 const elvenShieldRecipe = {
-    leatherStrips: 2,
-    ironIngot: 1,
-    refinedMoonstone: 4,
-  };
-  
-  const elvenGauntletsRecipe = {
-    ...elvenShieldRecipe,
-    leather: 1,
-    refinedMoonstone: 4,
-  };
-  
-  console.log(elvenShieldRecipe);
-  console.log(elvenGauntletsRecipe);
-  
-  import { useState } from 'react';
-  
-  const Recipes = () => {
-    const [recipe, setRecipe] = useState({});
+  leatherStrips: 2,
+  ironIngot: 1,
+  refinedMoonstone: 4,
+};
 
-    return (
-        <div>
-            <div>
-                <h3>Current Recipe</h3>
-                <button onClick={() => setRecipe(elvenShieldRecipe)}>Elven Shield Recipe</button>
-                <button onClick={() => setRecipe(elvenGauntletsRecipe)}>Elven Gauntlet Recipe</button>
-            </div>
+const elvenGauntletsRecipe = {
+  ...elvenShieldRecipe,
+  leather: 1,
+  refinedMoonstone: 4,
+};
 
-            <ul>
-                {Object.keys(recipe).map((material) => <li key={material}>{material}: {recipe[material]}</li>)}
-            </ul>        
-        </div>
+console.log(elvenShieldRecipe);
+console.log(elvenGauntletsRecipe);
 
-    )
-  }
-  
-  export default Recipes
+// @refresh reset
+import { useState } from 'react';
+
+const Recipes = () => {
+  const [recipe, setRecipe] = useState({});
+
+  return (
+    <div>
+      <div>
+        <h3>Current Recipe</h3>
+        <button onClick={() => setRecipe(elvenShieldRecipe)}>
+          Elven Shield Recipe
+        </button>
+        <button onClick={() => setRecipe(elvenGauntletsRecipe)}>
+          Elven Gauntlet Recipe
+        </button>
+      </div>
+
+      <ul>
+        {Object.keys(recipe).map((material) => (
+          <li key={material}>
+            {material}: {recipe[material]}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Recipes;
